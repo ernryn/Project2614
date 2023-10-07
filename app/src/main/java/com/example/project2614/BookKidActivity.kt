3fa0369a97376c3367ad5a15a87f5b023641f435
LENOVO <ernariyanti07@gmail.com>
2023-10-08 00:14:21 +0700

    update fragments

---
+++ app/src/main/java/com/example/project2614/BookKidActivity.kt
@@ -0,0 +1,40 @@
+package com.example.project2614
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.project2614.databinding.ActivityBookKidBinding
+
+class BookKidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_kid)
+
+        //INSTANCE
+        val btnFairy:Button = findViewById(R.id.buttonFairy)
+        val btnFable:Button = findViewById(R.id.buttonFable)
+        val btnScience:Button = findViewById(R.id.buttonScience)
+
+        //event button fairy
+        btnFairy.setOnClickListener {
+            replaceFragment(FairyFragment())
+        }
+        //event button fairy
+        btnFable.setOnClickListener{
+            replaceFragment(FableFragment())
+        }
+        //event button fairy
+        btnScience.setOnClickListener{
+            replaceFragment(ScienceFragment())
+        }
+    }
+
+    //create method replace fragment
+    private fun replaceFragment(frg:Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTrx = fragmentManager.beginTransaction()
+        fragmentTrx.replace(R.id.fragmentKidBook,frg)
+        fragmentTrx.commit()
+    }
+}
--- app/src/main/java/com/example/project2614/HomeActivity.kt
+++ app/src/main/java/com/example/project2614/HomeActivity.kt
@@ -1,11 +1,19 @@
 package com.example.project2614
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val btnBookKidActivity: Button = this.findViewById(R.id.buttonMenu1)
+
+        btnBookKidActivity.setOnClickListener {
+            val intent = Intent(this, BookKidActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
