b69bfa64ed3bf31f100c9711ad625dabbc76bee7
amikom <ernariyanti07@gmail.com>
2023-10-11 14:59:00 +0700

    update recycler view

--- app/src/main/java/com/example/project2614/HomeActivity.kt
+++ app/src/main/java/com/example/project2614/HomeActivity.kt
@@ -3,13 +3,32 @@ package com.example.project2614
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val btnBookKidActivity: Button = this.findViewById(R.id.buttonMenu1)
+        val rvBuku:RecyclerView = findViewById(R.id.recyclerViewBuku)
+
+        //set layout manager di RecyclerView
+        rvBuku.layoutManager = LinearLayoutManager(this)
+        //list data buku
+        val data = ArrayList<BukuModel>()
+        data.add(BukuModel(R.drawable.book1,"Emi's Beach Adventure",
+                        "Lorem ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book2,"Emi's Beach Adventure",
+            "Lorem ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        data.add(BukuModel(R.drawable.book4,"Emi's Beach Adventure",
+            "Lorem ipsum Dolor Sit Amet Lorem Ipsum Dolor Sit Amet"))
+        //set adapter
+        val adapter = AdapterHome(data)
+        //set adapter ke recycler view
+        rvBuku.adapter = adapter
 
         btnBookKidActivity.setOnClickListener {
             val intent = Intent(this, BookKidActivity::class.java)
